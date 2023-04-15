c64381d3a853bfa79737d99f08906429303a1865
Franchesca Marie Donadillo <franchescamariedonadillo@gmail.com>
2023-04-15 22:35:51 +0800

    importing design

--- decryptor.py
+++ decryptor.py
@@ -3,7 +3,14 @@
 # ASSIGNMENT 2_Problem 2 - Decryption
 
 # import for design
+from rich.console import Console
+from rich.theme import Theme
+from rich import print
+
 # encode title
+design = Theme({"title": "bold magenta", "decrypt": "underline bold yellow", "blinky": "bold green blink"})
+console = Console(theme = design)
+
 # ask user input
 # loop all throughout the user input
 # change * into a
