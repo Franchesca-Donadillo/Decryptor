dc2ee100ee01c719ad25ad235dc0999cce3dcdfd
Franchesca Marie Donadillo <franchescamariedonadillo@gmail.com>
2023-04-15 22:15:45 +0800

    pseudocode

---
+++ decryptor.py
@@ -0,0 +1,15 @@
+# Franchesca Marie Donadillo
+# BSCPE 1-5
+# ASSIGNMENT 2_Problem 2 - Decryption
+
+# import for design
+# encode title
+# ask user input
+# loop all throughout the user input
+# change * into a
+# change & into e
+# change # into i
+# change + into o
+# change ! into u
+# add partition
+# print the decrypted text of the user
